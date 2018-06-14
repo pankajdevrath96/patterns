51aa5db0c10a399746212f8386be234995fff33f
kriti-garg <kritigarg1998@gmail.com>
2018-06-14 15:35:28 +0530

    three patterns

---
+++ Pattern4.java
@@ -0,0 +1,16 @@
+public class Pattern4{
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=5;j++){
+				if(j<=i && j<=10-i){
+					System.out.print("*");
+				}
+					else
+						System.out.print(" ");
+				
+				
+			}
+			System.out.println(" ");
+		}
+	}
+}
